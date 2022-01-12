758496ab09dc6c55c999adc8f049619ec34d5d6d
Ayush Gupta <70760483+fitgang@users.noreply.github.com>
2022-01-12 18:34:44 +0530

    Delete semui.js

--- semui.js
+++
@@ -1,8 +0,0 @@
-// Initialise accordion in the 'my-tech' section
-$('.ui.accordion').accordion();
-
-// Initialise radio buttons in the review form
-$('.ui.radio.checkbox').checkbox();
-
-// Initialise rating in the review form
-$('.ui.rating').rating();
